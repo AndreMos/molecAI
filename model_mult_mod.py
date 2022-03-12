4b34204f9d392564e975f710440eeae56ca1bf0d
Andrey Mossyayev <AndreMos@87-77-196-226.mna.fu-berlin.de>
2022-03-12 03:20:31 +0300

    multi_mod_transf_add_helper_class_for_modal_att_mask

--- model_mult_mod.py
+++ model_mult_mod.py
@@ -7,6 +7,8 @@ from torch_scatter import scatter
 import pytorch_lightning  as pl
 import numpy as np
 from transformers import BertModel, BertConfig, MMBTConfig, MMBTForClassification
+from transformers.modeling_outputs import SequenceClassifierOutput
+from torch.nn import CrossEntropyLoss, MSELoss
 
 
 class RbfExpand(nn.Module):
@@ -27,6 +29,70 @@ class RbfExpand(nn.Module):
     def forward(self, distances):
         distances = distances.unsqueeze(-1)
         return torch.exp(-self.gamma * torch.pow((distances - self.spaced_values), 2))
+
+class Helper(MMBTForClassification):
+
+  def forward(
+          self,
+          input_modal,
+          input_ids=None,
+          modal_start_tokens=None,
+          modal_end_tokens=None,
+          attention_mask=None,
+          token_type_ids=None,
+          modal_token_type_ids=None,
+          position_ids=None,
+          modal_position_ids=None,
+          head_mask=None,
+          inputs_embeds=None,
+          labels=None,
+          return_dict=None,
+          encoder_attention_mask=None
+      ):
+          return_dict = return_dict if return_dict is not None else self.config.use_return_dict
+
+          outputs = self.mmbt(
+              input_modal=input_modal,
+              input_ids=input_ids,
+              modal_start_tokens=modal_start_tokens,
+              modal_end_tokens=modal_end_tokens,
+              attention_mask=attention_mask,
+              token_type_ids=token_type_ids,
+              modal_token_type_ids=modal_token_type_ids,
+              position_ids=position_ids,
+              modal_position_ids=modal_position_ids,
+              head_mask=head_mask,
+              inputs_embeds=inputs_embeds,
+              return_dict=return_dict,
+              encoder_attention_mask=encoder_attention_mask
+          )
+
+          pooled_output = outputs[1]
+
+          pooled_output = self.dropout(pooled_output)
+          logits = self.classifier(pooled_output)
+
+          loss = None
+          if labels is not None:
+              if self.num_labels == 1:
+                  #  We are doing regression
+                  loss_fct = MSELoss()
+                  loss = loss_fct(logits.view(-1), labels.view(-1))
+              else:
+                  loss_fct = CrossEntropyLoss()
+                  loss = loss_fct(logits.view(-1, self.num_labels), labels.view(-1))
+
+          if not return_dict:
+              output = (logits,) + outputs[2:]
+              return ((loss,) + output) if loss is not None else output
+
+          return SequenceClassifierOutput(
+              loss=loss,
+              logits=logits,
+              hidden_states=outputs.hidden_states,
+              attentions=outputs.attentions,
+          )
+
 
 
 class MultiMod(pl.LightningModule):
