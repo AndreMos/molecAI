7c4c250efc44e4c6dad0b7237553550260e2295c
Andrey Mossyayev <AndreMos@87-77-197-210.mna.fu-berlin.de>
2022-04-03 22:12:57 +0300

    perceiverangleswithx2latentand6heads

--- main.py
+++ main.py
@@ -62,7 +62,7 @@ data_module = DataModule()
 # train
 
 config = PerceiverConfig(
-    num_latents=64, d_latents=128, num_labels=1, num_cross_attention_heads=6, num_self_attends_per_block = 10,
+    num_latents=64, d_latents=128, num_labels=1, num_cross_attention_heads=4, num_self_attends_per_block = 10,
      attention_probs_dropout_prob=0.1, num_self_attention_heads=8
 )
 decoder = PerceiverClassificationDecoder(
