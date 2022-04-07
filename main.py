a181b931d1b3bd12c79fddf95414209e3b54262f
Andrey Mossyayev <AndreMos@87-77-192-198.mna.fu-berlin.de>
2022-04-07 17:52:58 +0300

    perceiverangles1head

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 #! /usr/bin/env python3
-#SBATCH -o perceiverangles2xlat4headsfixeddict
+#SBATCH -o perceiverangles2xlat1head
 #SBATCH -p gpu
 #SBATCH -D /data/scratch/andrem97
 #SBATCH --gres=gpu:1
@@ -63,7 +63,7 @@ data_module = DataModule()
 # train
 
 config = PerceiverConfig(
-    num_latents=64, d_latents=128, num_labels=1, num_cross_attention_heads=4, num_self_attends_per_block = 10,
+    num_latents=64, d_latents=128, num_labels=1, num_cross_attention_heads=1, num_self_attends_per_block = 10,
      attention_probs_dropout_prob=0, num_self_attention_heads=8
 )
 decoder = PerceiverClassificationDecoder(
