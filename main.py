befb094156bc3de341acbd5696bc18e6cddf5714
Andrey Mossyayev <AndreMos@87-77-196-205.mna.fu-berlin.de>
2022-04-19 15:54:18 +0200

    hydraapplied

--- main.py
+++ main.py
@@ -13,6 +13,7 @@ import os
 sys.path.append(os.getcwd())
 
 import torch.nn as nn
+import hydra
 from transformers import (
     PerceiverForSequenceClassification,
     PerceiverConfig,
@@ -47,32 +48,41 @@ dataset = CustomDataset(
 
 
 class DataModule(pl.LightningDataModule):
-    def train_dataloader(self):
+    @hydra.main(config_path="configs", config_name="config")
+    def train_dataloader(self, config):
         return torch_geometric.loader.DataLoader(
-            dataset[:110462], shuffle=True, batch_size=32, **{"drop_last": True}
+            dataset[:110462], shuffle=True, batch_size=config.perceiver.batch_size, **{"drop_last": True}
         )
 
-    def val_dataloader(self):
+    @hydra.main(config_path="configs", config_name="config")
+    def val_dataloader(self, config):
         return torch_geometric.loader.DataLoader(
-            dataset[110462:111462], batch_size=32, **{"drop_last": True}
+            dataset[110462:111462], batch_size=config.perceiver.batch_size, **{"drop_last": True}
         )
 
 
 data_module = DataModule()
 
 # train
+@hydra.main(config_path="configs", config_name="config")
+def load_config(config):
+    allias = config.repceiver.config
+    perc_conf = PerceiverConfig(
+    num_latents=allias.num_latents, d_latents=allias.d_latents, num_labels=allias.num_labels, num_cross_attention_heads=allias.num_cross_attention_heads, num_self_attends_per_block = allias.num_self_attends_per_block,
+     attention_probs_dropout_prob=allias.attention_probs_dropout_prob, num_self_attention_heads=allias.num_self_attention_heads
+    )
+    min_padding_size = config.repceiver.min_padding_size
+    return perc_conf, min_padding_size
 
-config = PerceiverConfig(
-    num_latents=64, d_latents=128, num_labels=1, num_cross_attention_heads=8, num_self_attends_per_block = 10,
-     attention_probs_dropout_prob=0, num_self_attention_heads=8
-)
+config, min_padding_size = load_config()
+
 decoder = PerceiverClassificationDecoder(
     config,
     num_channels=config.d_latents,
     trainable_position_encoding_kwargs=dict(num_channels=config.d_latents, index_dims=1),
     use_query_residual=True,
 )
-preprocessor = PerceiverMultimodalPreprocessor(min_padding_size=2,
+preprocessor = PerceiverMultimodalPreprocessor(min_padding_size=min_padding_size,
                          modalities=nn.ModuleDict({
                              'angles': AnglePreprocessor(),
                              'dist' : MolecPreprocessor()
